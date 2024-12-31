7481eecb43ef4c3bdba2e6914de4c0f28a3eca75
Zapzarapza <marko.oreskovic1995@gmail.com>
2024-12-31 18:11:42 +0100

    Login window as landing page

--- wedding/views.py
+++ wedding/views.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.shortcuts import render
 from guests.save_the_date import SAVE_THE_DATE_CONTEXT_MAP
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def home(request):
     return render(request, 'home.html', context={
         'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
